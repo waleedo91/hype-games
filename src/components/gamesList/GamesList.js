681b5a3807374e731f2e38b5ae75f700d2ba82c4
Waleed <wsaleh1991@gmail.com>
2021-11-21 18:47:14 -0600

    working on the responsive aspect of the main page

--- src/components/gamesList/GamesList.js
+++ src/components/gamesList/GamesList.js
@@ -1,16 +1,18 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 // styles
 import "./GamesList.css";
 
 export default function GamesList({ games }) {
+  console.log(games);
   return (
     <div className="games-list">
       {games.length === 0 && <p>No Games</p>}
       {games.map((game) => (
-        <div className="game-container" key={game.id}>
-          <h4>{game.name}</h4>
-          <Link to={`/game/${game.id}`} key={game.id}>
+        <div className="games-container">
+          <h4>{game.title}</h4>
+          <Link to={`/game/${game.id}`}>
             <img
               className="game-image"
               src={game.image}
--- src/pages/game/GameSummary.js
+++ src/pages/game/GameSummary.js
@@ -4,44 +4,7 @@ import "./Game.css";
 export default function GameSummary({ game }) {
   return (
     <div>
-      <div className="game-summary">
-        <h2 className="game-title">{game.name}</h2>
-        <img className="game-image" src={game.image} alt="game pic" />
-      </div>
-      <div className="game-container">
-        <div className="game-left">
-          <p className="game-description">{game.about}</p>
-        </div>
-        <div className="game-right">
-          <p className="metascore">Metascore: {game.metascore}</p>
-
-          <ul className="platforms">
-            <span>Platforms:</span>
-            {game.platforms.map((p) => (
-              <li key={p}>{p}</li>
-            ))}
-          </ul>
-          <br />
-          <ul className="genres">
-            {game.genre.map((g) => (
-              <li key={g}>{g}</li>
-            ))}
-          </ul>
-          <br />
-
-          {game.developer.map((d) => (
-            <p className="developers" key={d}>
-              {d}
-            </p>
-          ))}
-          <br />
-
-          <p className="rating">{game.rating}</p>
-
-          <br />
-          <p className="release">{game.release}</p>
-        </div>
-      </div>
+      <h2 style={{ color: "white" }}>Game Detail</h2>
     </div>
   );
 }
