68d57d2b3aeb3a1c839e8dcf806bc8f762364204
Waleed <wsaleh1991@gmail.com>
2021-11-24 01:23:54 -0600

    added style, next adding filter buttons

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -1,14 +1,12 @@
 import { useCollection } from "../../hooks/useCollection";
 import GameList from "../../components/gamesList/GamesList";
-import { useState } from "react";
-import { useAuthContext } from "../../hooks/useAuthContext";
+
 
 // styles
 import "./Home.css";
 
 export default function Home() {
   const { documents, error } = useCollection("games");
-  const { user } = useAuthContext();
 
   return (
     <div>
